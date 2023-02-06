91cf7538f9d73654be8af67ad10aa7a752c3404a
Rusisg <rasul.nulrybekov@gmail.com>
2023-02-07 00:59:11 +0500

    backend progress

--- awd.md
+++ awd.md
@@ -1,5 +1,5 @@
 # GUW Bugin
 ## News website
-1. deal with htmls
+1. need to set the html templates
 2. how to scrape from this [website](https://tengrinews.kz/tag/%D0%B0%D1%82%D1%8B%D1%80%D0%B0%D1%83/)
 3. Logo :)
--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"net/http"
+	"text/template"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.Static("/css", "./public/css")
-	r.LoadHTMLFiles("./public/html/index.html")
+	tmpl := template.Must(template.ParseFiles("public/html/base.html", "public/html/header.html", "public/html/index.html", "public/html/news.html", "public/html/news_detail.html"))
 
-	r.GET("/", index)
+	r.Static("/css", "./public/css")
+	r.LoadHTMLGlob("./public/html/*.html")
+
+	r.GET("/news", news)
+	r.GET("/", func(c *gin.Context) {
+		renderTemplate(c, tmpl, gin.H{
+			"title": "Atyrau Bugin",
+		})
+	})
+
 	r.Run(":3000")
 }
 
-func index(c *gin.Context) {
-	c.HTML(200, "index.html", nil)
+func news(c *gin.Context) {
+	c.HTML(http.StatusOK, "news.tmpl", gin.H{
+		"title": "Latest News",
+		"articles": []interface{}{
+			map[string]interface{}{
+				"title":   "Article 1",
+				"content": "This is the first article",
+			},
+			map[string]interface{}{
+				"title":   "Article 2",
+				"content": "This is the second article",
+			},
+		},
+	})
+}
+
+func renderTemplate(c *gin.Context, tmpl *template.Template, data interface{}) {
+	err := tmpl.Execute(c.Writer, data)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
 }
